92d9a67f100afac01b6b1333b26fbce1069f7d95
DaviAtrock <daviatrock@gmail.com>
2023-06-22 00:25:06 -0300

    Carrinho finalizado com contexto

--- ProjetoDigitech/src/contexts/ProdutoContext.jsx
+++ ProjetoDigitech/src/contexts/ProdutoContext.jsx
@@ -1,44 +1,51 @@
-import { createContext, useState } from "react"
-import { listaProdutos } from "../services/CartService"
+import { createContext, useState } from "react";
+import { listaProdutos } from "../services/CartService";
 
 const ProdutoContext = createContext({
   produtos: [],
   produtoCarrinho: [],
   listarProdutos: () => { },
   adicionaProdutoCarrinho: () => { },
-})
+  removerProduto: () => { },
+});
 
 export function ProdutoContextProvider(props) {
-  const [produtos, setProdutos] = useState([])
-
-  const [produtoCarrinho, setProdutoCarrinho] = useState([])
+  const [produtos, setProdutos] = useState([]);
+  const [produtoCarrinho, setProdutoCarrinho, removerProdutoCarrinho] = useState([]);
 
   async function listarProdutos() {
     try {
-      const data = await listaProdutos()
-      setProdutos(data)
+      const data = await listaProdutos();
+      setProdutos(data);
     } catch (error) {
-      throw new Error(error.message)
+      throw new Error(error.message);
     }
   }
 
   function adicionaProdutoCarrinho(produto) {
     setProdutoCarrinho((prevProdutosCarrinho) => [...prevProdutosCarrinho, produto]);
-  }  
+  }
+
+  function removerProduto(produtoId) {
+    setProdutoCarrinho((prevProdutosCarrinho) =>
+      prevProdutosCarrinho.filter((produto) => produto.id !== produtoId)
+    );
+  }
+  
 
   const contexto = {
     produtos,
     produtoCarrinho,
     listarProdutos,
     adicionaProdutoCarrinho,
-  }
+    removerProduto,
+  };
 
   return (
     <ProdutoContext.Provider value={contexto}>
       {props.children}
     </ProdutoContext.Provider>
-  )
-
+  );
 }
 
 export default ProdutoContext;
--- ProjetoDigitech/src/pages/Carrinho.jsx
+++ ProjetoDigitech/src/pages/Carrinho.jsx
@@ -1,15 +1,46 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { BsFileMinus, BsFilePlus, BsFillFileExcelFill } from 'react-icons/bs';
 import './Carrinho.css';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import ProdutoContext from '../contexts/ProdutoContext';
 
 export default function Carrinho() {
-  const { produtoCarrinho } = useContext(ProdutoContext);
+  const { produtoCarrinho, removerProduto } = useContext(ProdutoContext);
+  const [quantidades, setQuantidades] = useState({});
 
   useEffect(() => {
-    console.log(produtoCarrinho)
+    console.log(produtoCarrinho);
   }, []);
+
+  function incrementarQuantidade(produtoId) {
+    setQuantidades((prevQuantidades) => ({
+      ...prevQuantidades,
+      [produtoId]: (prevQuantidades[produtoId] || 1) + 1,
+    }));
+  }
+
+  function decrementarQuantidade(produtoId) {
+    setQuantidades((prevQuantidades) => {
+      const quantidadeAtual = prevQuantidades[produtoId] || 1;
+      if (quantidadeAtual > 1) {
+        return {
+          ...prevQuantidades,
+          [produtoId]: quantidadeAtual - 1,
+        };
+      } else {
+        const { [produtoId]: _, ...rest } = prevQuantidades;
+        return rest;
+      }
+    });
+  }
+
+  const subTotal = produtoCarrinho.reduce((total, produto) => {
+    const quantidade = quantidades[produto.id] || 1;
+    return total + produto.preco * quantidade;
+  }, 0);
+
+  const total = subTotal;
 
   return (
     <>
@@ -40,45 +71,46 @@ export default function Carrinho() {
                         </div>
                       </div>
                     </td>
-                    <td>R$ {produtoNoCarrinho.preco}</td>
+                    <td>R$ {produtoNoCarrinho.preco.toFixed(2)}</td>
                     <td>
                       <div className="qty">
-                        <button>-</button>
-                        <span>1</span>
-                        <button>+</button>
+                        <span>
+                          <BsFileMinus onClick={() => decrementarQuantidade(produtoNoCarrinho.id)} />
+                        </span>
+                        <span>{quantidades[produtoNoCarrinho.id] || 1}</span>
+                        <span>
+                          <BsFilePlus onClick={() => incrementarQuantidade(produtoNoCarrinho.id)} />
+                        </span>
                       </div>
                     </td>
-                    <td>R$ {produtoNoCarrinho.preco}</td>
+                    <td>R$ {(produtoNoCarrinho.preco * (quantidades[produtoNoCarrinho.id] || 1)).toFixed(2)}</td>
                     <td>
-                      <button className="remove">Remover</button>
+                      <span>
+                        <BsFillFileExcelFill onClick={() => removerProduto(produtoNoCarrinho.id)} />
+                      </span>
                     </td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </section>
+
           <aside>
             <div className="box">
               <header className="header-resumo">Resumo da compra</header>
               <div className="info">
                 <div>
                   <span>Sub-total</span>
-                  <span>R$ {produtoCarrinho.length > 0 ? produtoCarrinho[0].preco : '0.00'}</span>
+                  <span>R$ {subTotal.toFixed(2)}</span>
                 </div>
                 <div>
                   <span>Frete</span>
                   <span>Gratuito</span>
                 </div>
-                <div>
-                  <button>
-                    Adicionar cupom de desconto
-                    <i className="bx bx-right-arrow-alt"></i>
-                  </button>
-                </div>
               </div>
               <footer className="footer-compra">
                 <span>Total</span>
-                <span>R$ {produtoCarrinho.length > 0 ? produtoCarrinho[0].preco : '0.00'}</span>
+                <span>R$ {total.toFixed(2)}</span>
               </footer>
             </div>
             <button className="finalizar-compra">Finalizar Compra</button>
